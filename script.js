55248320ad8c7df4362f904348f7fe1143690920
Priyank Bansal <165632145+Bansal-priyank@users.noreply.github.com>
2025-08-13 09:34:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById("myform").addEventListener("submit", function(e){
+	e.preventDefault();
+
+let firstName = document.getElementById("firstName").value;
+let lastName = document.getElementById("lastName").value;
+let phoneNumber = document.getElementById("phoneNumber").value;
+let emailId = document.getElementById("emailId").value;
+
+alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`);
+
+});
